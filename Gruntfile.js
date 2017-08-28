e051424c013f736f9ed16b27496493750e3256a9
monialisa91 <monikarichter18@gmail.com>
2017-08-28 13:58:43 +0200

    Tabs instead of spaces in Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,61 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-  	sass: {
-  		options: {
-  			sourceMap: true
-  		},
-  		dist: {
-  			files: {
-  				'main.css': 'main.sass'
-  			}
-  		}
-  	},
+// Project configuration.
 
-  	imagemin: {
-  		dynamic: {
-  			files: [{
-  				expand: true,
-  				cwd: 'images/',
-  				src: ['**/*.{png,jpg,gif}'],
-  				dest: 'images/build/'
-  			}]
-  		}
-  	},
-  	browserSync: {
-    	dev: {
-        	bsFiles: {
-	            src : [
-	                '/css/*.css'
-	            ]
-	        },
-	        options: {
-	            baseDir: "./"
-	        }
-		}
-	},
-  	watch: {
-    	scripts: {
-        	files: ['sass/*.sass'],
-        	tasks: ['sass'],
-        	options: {
-            	spawn: false,
-        	},
-    	}
-	}
+	grunt.initConfig({
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'style.css': 'style.sass'
+				}
+			}
+		},
 
-	  });
-  // Load the plugins tasks
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-browser-sync');
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'images/build/'
+				}]
+			}
+		},
+
+		browserSync: {
+			dev: {
+				bsFiles: {
+					src : [
+						'/css/*.css'
+					]
+				},
+				options: {
+					baseDir: "./"
+				}
+			}
+		},
+
+		watch: {
+			scripts: {
+				files: ['sass/*.sass'],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				},
+			}
+		},
+	});		
+
+	// Load the plugins tasks
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
 
 
-  // Default task(s).
-  grunt.registerTask('default', ["sass","imagemin","watch","browserSync"]);
+	// Default task(s).
+	grunt.registerTask('default', ["sass", "imagemin", "watch", "browserSync"]);
 };
